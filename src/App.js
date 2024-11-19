556efbf5acd71823e30025a9fb958a4cd56b60d2
Adilxa <adilhanimerov82@gmail.com>
2024-11-19 22:33:07 +0600

    feat: initai;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import SettingsPage from './pages/SettingsPage';
+import NotFound from './pages/NotFound';
+import Contacts from './pages/Contacts';
+import FormPage from './pages/FormPage';
+import "./App.css";
+
+const IdPage = () => {
+
+  const {id} = useParams()
+
+  return (
+    <h1>{id}</h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path='/contacts' element={<Contacts/>}/>
+        <Route path='/form' element={<FormPage/>}/>
+        <Route path='/tasks/:id' element={<IdPage/> }/>
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
